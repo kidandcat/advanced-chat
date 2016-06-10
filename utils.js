38a3891d2fbf4b5b1decaa8dd21c622801ada73b
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-06-10 17:32:38 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,31 +1,44 @@
 (function(utils) {
 
     utils.observe = function(origin, destiny) {
-        return _observe(origin, destiny, true);
+        return _observe(origin, destiny, true, false);
     }
 
-    utils.observeN = function(origin, destiny) {
-        return _observe(origin, destiny, false);
+    utils.Nobserve = function(origin, destiny) {
+        return _observe(origin, destiny, false, false);
     }
 
-    function _observe(origin, destiny, compare) {
+    utils.observeEval = function(origin, destiny) {
+        return _observe(origin, destiny, true, true);
+    }
+
+    utils.NobserveEval = function(origin, destiny) {
+        return _observe(origin, destiny, false, true);
+    }
+
+    function _observe(origin, destiny, compare, eva) {
         var success = function(c) {};
         var error = function(c) {};
         //Promise
 
+
+        var done = false;
         var timer = setInterval(function() {
             if (compare) {
-                if (origin === destiny) {
+                if ((eva)?eval(origin):origin === destiny && !done) {
+                    done = true;
                     clearInterval(timer);
                     success(origin);
                 }
             } else {
-                if (origin !== destiny) {
+                if ((eva)?eval(origin):origin !== destiny && !done) {
+                    done = true;
                     clearInterval(timer);
                     success(origin);
                 }
             }
         }, 200);
+
 
         //Promise
         return {
