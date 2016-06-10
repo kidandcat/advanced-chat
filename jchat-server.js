2cecd20b2e6f5b1c84901cbcef6713f2a19a3837
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-06-10 17:32:10 +0200

    Update jchat-server.js

--- jchat-server.js
+++ jchat-server.js
@@ -3,6 +3,7 @@ var io = require('socket.io')(8282);
 
 io.on('connect', function(socket){
     socket.on('chat:login', function(data){
+        console.log(data);
         socket.nick = data.nick;
         socket.join(data.room);
         socket.room = data.room;
