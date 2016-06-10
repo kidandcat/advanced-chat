2ea79a0f5803e83d6b9572293264b7d59628c127
Jairo Caro-Accino Viciana <kidandcat@gmail.com>
2016-06-10 17:31:55 +0200

    Update jchat-browser.js

--- jchat-browser.js
+++ jchat-browser.js
@@ -1,22 +1,27 @@
-jchat = (function(conf) {
+jchat = (function() {
     var obj = {};
     var state = 'down';
     var container;
+    var room = null;
+    var user = null;
     obj.config = {};
     obj.utils = {};
-
+    obj.remote = {};
 
 
 
     /*  *  *  *  *  *  *
      *  begin CONFIG   *
      *  *  *  *  *  *  */
-    conf = conf || {};
-    obj.config.title = conf.title || 'Chat live';
-    obj.config.socketUrl = conf.socketUrl || 'http://192.168.0.12:8282';
-    /*  *  *  *  *  *
-     *  end CONFIG  *
-     *  *  *  *  *  */
+    obj.init = function(conf) {
+            conf = conf || {};
+            obj.config.title = conf.title || 'Chat live';
+            obj.config.socketUrl = conf.socketUrl || 'http://192.168.0.12:8282';
+            _init();
+        }
+        /*  *  *  *  *  *
+         *  end CONFIG  *
+         *  *  *  *  *  */
 
 
 
@@ -25,11 +30,15 @@ jchat = (function(conf) {
     /*  *  *  *  *  *
      *  begin INIT  *
      *  *  *  *  *  */
-    loadSocketLibrary().then(function(){
-      loadSocketConnection();
-      containerCreator();
-      firstShow();
-    });
+    function _init() {
+        loadSocketLibrary().then(function() {
+            loadSocketConnection().then(function() {
+                registerSocketListeners();
+                containerCreator();
+                firstShow();
+            });
+        });
+    }
     /*  *  *  *  *
      *  end INIT *
      *  *  *  *  */
@@ -41,20 +50,21 @@ jchat = (function(conf) {
     /*  *  *  *  *  *  *
      *  begin SOCKET   *
      *  *  *  *  *  *  */
-    function loadSocketLibrary(){
-      var success = function(c){};
-      var error = function(c){};
-      //Promise
+    function loadSocketLibrary() {
+        var success = function(c) {};
+        var error = function(c) {};
+        //Promise
 
-      var url = obj.config.socketUrl + '/socket.io/socket.io.js';
-      var elem = document.createElement('script');
-      elem.src = url;
-      document.querySelector('head').appendChild(elem);
-      utils.observeN(window.io, null).then(function(){
-        success(true);
-      });
+        var url = obj.config.socketUrl + '/socket.io/socket.io.js';
+        var elem = document.createElement('script');
+        elem.src = url;
+        document.querySelector('head').appendChild(elem);
+        utils.Nobserve(window.io, null).then(function() {
+            console.log('io ready!');
+            success(true);
+        });
 
-      //Promise
+        //Promise
         return {
             then: function(cb) {
                 success = cb;
@@ -69,15 +79,18 @@ jchat = (function(conf) {
 
 
 
-    function loadSocketConnection(){
-      var success = function(c){};
-      var error = function(c){};
-      //Promise
+    function loadSocketConnection() {
+        var success = function(c) {};
+        var error = function(c) {};
+        //Promise
 
-      obj.connection = io(obj.config.socketUrl);
+        obj.connection = io(obj.config.socketUrl);
+        utils.observeEval('jchat.connection.connected', true).then(function() {
+            console.log('conenction ready!');
+            success(true);
+        });
 
-
-      //Promise
+        //Promise
         return {
             then: function(cb) {
                 success = cb;
@@ -91,19 +104,28 @@ jchat = (function(conf) {
     }
 
 
-    function registerSocketListeners(){
-      obj.remote = {};
-      obj.remote.login = function(nick, sala){
-          obj.connection.emit('chat:login', { nick: nick, room: sala });
-      }
+    function registerSocketListeners() {
+        obj.remote.login = function(params) {
+            var room = params.room || Math.floor(Math.random() * 900000 + 100000);
+            user = params.nick || null;
+            obj.connection.emit('chat:login', {
+                nick: params.nick,
+                room: room
+            });
+        }
 
-      obj.remote.send = function(msg){
-          obj.connection.emit('chat:msg', { msg: msg });
-      }
+        obj.remote.send = function(msg) {
+            if (typeof user !== 'undefined') {
+                console.log('sending remote');
+                obj.connection.emit('chat:msg', {
+                    message: msg
+                });
+            }
+        }
 
-      obj.connection.on('chat:msg', function(data){
-          obj.remoteMessage(data.message, data.user);
-      });
+        obj.connection.on('chat:msg', function(data) {
+            obj.remoteMessage(data.message, data.user);
+        });
 
     }
     /*  *  *  *  *  *
@@ -114,27 +136,41 @@ jchat = (function(conf) {
 
 
 
-     /*  *  *  *  *  *  *  *
-      *  begin FUNCTIONS   *
-      *  *  *  *  *  *  *  */
-      obj.sendMessage = function(message){
+    /*  *  *  *  *  *  *  *
+     *  begin FUNCTIONS   *
+     *  *  *  *  *  *  *  */
+    obj.sendMessage = function(message) {
+        if (message != '') {
+            message = message.charAt(0).toUpperCase() + message.slice(1);
+            obj.localMessage(message);
+            obj.remote.send(message);
+        }
+    }
 
-      }
+    obj.sendPhantomMessage = function(message) {
+        obj.remote.send(message);
+    }
 
-      obj.remoteMessage = function(message, user){
+    obj.remoteMessage = function(message, user) {
+        container.childs.body.insertBefore(getOtherMessage(message), container.childs.body.firstChild);
+    }
 
-      }
+    obj.localMessage = function(message) {
+        container.childs.body.insertBefore(getOwnMessage(message), container.childs.body.firstChild);
+    }
 
-      obj.localMessage = function(message){
-
-      }
-
-      obj.login = function(data){
-
-      }
-     /*  *  *  *  *  *  *
-      *  end FUNCTIONS  *
-      *  *  *  *  *  *  */
+    obj.login = function(data) {
+            if (typeof data === 'string') {
+                obj.remote.login({
+                    nick: data
+                });
+            } else {
+                obj.remote.login(data);
+            }
+        }
+        /*  *  *  *  *  *  *
+         *  end FUNCTIONS  *
+         *  *  *  *  *  *  */
 
 
 
@@ -145,6 +181,7 @@ jchat = (function(conf) {
      *  *  *  *  *  */
     function containerCreator() {
         container = document.createElement('div');
+        container.childs = {};
         container.style.position = "fixed";
         container.style.bottom = '-25em';
         container.style.width = "25em";
@@ -170,22 +207,25 @@ jchat = (function(conf) {
             obj.toggle();
         });
         container.appendChild(header);
+        container.childs.header = header;
     }
 
     function addBody() {
         body = document.createElement('div');
         body.style.position = 'absolute';
         body.style.width = '100%';
-        body.style.height = '22em';
-        body.style.bottom = 0;
+        body.style.height = '18em';
+        body.style.bottom = '4em';
         body.style['background-color'] = "white";
         body.style['box-sizing'] = 'border-box';
         body.style.border = '0.2em solid #1f79b2';
+        body.style.overflow = 'auto';
         container.appendChild(body);
+        container.childs.body = body;
     }
 
     function addBottom() {
-        bottom = document.createElement('div');
+        bottom = document.createElement('div'); //creamos elemento
         bottom.style.position = 'absolute';
         bottom.style.width = '100%';
         bottom.style.height = '4em';
@@ -193,9 +233,14 @@ jchat = (function(conf) {
         bottom.style['box-sizing'] = 'border-box';
         bottom.style.border = '0.2em solid #1f79b2';
         bottom.style.display = 'flex';
-        bottom.appendChild(getInput());
-        bottom.appendChild(getInputButton());
+        var input = getInput(); //generamos el input element
+        container.childs.input = input; //guardamos el elemento input en una coleccion del container para facil acceso
+        bottom.appendChild(input); //añadimos el input al bottom del chat
+        var button = getInputButton();
+        container.childs.button = button;
+        bottom.appendChild(button);
         container.appendChild(bottom);
+        container.childs.bottom = bottom;
     }
 
     function getTitle(text) {
@@ -211,6 +256,30 @@ jchat = (function(conf) {
         return title;
     }
 
+    function getOwnMessage(text) {
+        var message = document.createElement('span');
+        message.style.display = 'inline-block';
+        message.style.color = 'black';
+        message.style.padding = '0.8em';
+        message.style.width = '100%';
+        message.style['text-align'] = 'right';
+        message.style['box-sizing'] = 'border-box';
+        message.innerHTML = text;
+        return message;
+    }
+
+    function getOtherMessage(text) {
+        var message = document.createElement('span');
+        message.style.display = 'inline-block';
+        message.style.color = '#1f79b2';
+        message.style.padding = '0.8em';
+        message.style.width = '100%';
+        message.style['text-align'] = 'left';
+        message.style['box-sizing'] = 'border-box';
+        message.innerHTML = text;
+        return message;
+    }
+
     function getInput() {
         var input = document.createElement('textarea');
         input.style.flex = '1';
@@ -219,6 +288,18 @@ jchat = (function(conf) {
         input.style.height = '100%';
         input.style.width = '100%';
         input.style['box-sizing'] = 'border-box';
+        //Might and MAGIC!!!
+        input.addEventListener('keypress', function(event) {
+            if ((event.which == 13 || event.keyCode == 13 || event.keyCode == 10) && event.ctrlKey && !event.shiftKey) {
+                obj.sendMessage(input.value);
+                input.value = '';
+                input.focus();
+            } else if ((event.which == 13 || event.keyCode == 13 || event.keyCode == 10) && event.ctrlKey && event.shiftKey) {
+                obj.sendPhantomMessage(input.value);
+                input.value = '';
+                input.focus();
+            }
+        });
         return input;
     }
 
@@ -234,6 +315,11 @@ jchat = (function(conf) {
         button.style['font-size'] = '2em';
         button.style.color = 'white';
         button.style.cursor = 'pointer';
+        button.addEventListener('click', function() {
+            obj.sendMessage(container.childs.input.value);
+            container.childs.input.value = '';
+            container.childs.input.focus();
+        });
         return button;
     }
 
@@ -253,12 +339,13 @@ jchat = (function(conf) {
                 state = 'down';
             }
         }
-    /*  *  *  *  *
-     *  end HTML *
-     *  *  *  *  */
+        /*  *  *  *  *
+         *  end HTML *
+         *  *  *  *  */
 
 
     return obj;
-});
+})();
 
-jchat();
+
+jchat.init();
